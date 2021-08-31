3b417fe14dea384cf826ee2cd07b3a3350b07cc5
MorenaFischione <fischione.morena@gmail.com>
2021-08-31 16:34:17 +0200

    esercizio parola palindroma

---
+++ js/my_script.js
@@ -0,0 +1,34 @@
+/**
+ * Palidroma
+ *  1. Chiedere all’utente di inserire una parola
+ *  2. Creare una funzione per capire se la parola inserita è palindroma
+ */
+
+/**
+ * creo una funzione che verifica se la parola è palindroma con risultato booleano
+ * chiedo all'utente di inserire una parola
+ * se la parola inserita è palindroma stampo alert "la parola inserita è palindroma"
+ * se la parola inserita non è palindroma stampo alert "mi dispiace. la parola inserita non è palindroma"
+ */
+
+
+// function parolaPalindroma (parola) {
+//     if(parola.split("").reverse().join("") === parola){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// let parolaUtente = prompt("inserisci un parola Palindroma");
+// console.log(parolaUtente);
+
+// if (parolaPalindroma(parolaUtente)) {
+//     alert("la parola inserita è palindroma");
+// } else {
+//     alert("mi dispiace. la parola inserita non è palindroma");
+// }
+
+
+
+
